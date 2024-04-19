664e8349fdfe9dd1c8b58efd0b12472bb37dc348
Vinicius Carneiro Charleaux <vinicius.charleaux@gmail.com>
2024-04-19 17:52:09 -0300

    campos do formulario

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -14,7 +14,7 @@ export type InputControlProps = ComponentProps<'input'>
 export const InputControl:React.FC<InputControlProps> = ({...props}) => {
     return (
         <input 
-            className="flex-1border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" 
+            className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" 
             placeholder="Search"
             {...props}
         />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Mail } from "lucide-react";
 import { SettingTabs } from "./components/SettingsTabs";
+import {InputControl, InputPrefix, InputRoot} from '@/app/components/Input'
 
 export default function Home() {
   return (
@@ -10,7 +12,6 @@ export default function Home() {
         <div className="flex justify-between items-center pb-5 border-b border-zinc-200">
           <div className="space-y-1">
             <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
-            <span className="text-sm text-zinc-500">Update your photo and personal details here.</span>
           </div>
           <div className="flex items-center gap-2">
               <button type="button" className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50">Cancel</button>
@@ -18,10 +19,89 @@ export default function Home() {
           </div>
         </div>
 
-        <form id="settings" action="" className="mt-6 flex flex-col w-full">
+        <form id="settings" action="" className="mt-6 flex flex-col w-full gap-5 divide-y divide-zinc-200">
+          <div className="grid grid-cols-form gap-3">
+            <label htmlFor="firstName" className="text-sm font-medium text-zinc-700">Name</label>
+            <div className="grid gap-6 grid-cols-2">
+              <InputRoot>
+                <InputControl id="firstName" defaultValue="Vinicius"/>
+              </InputRoot>
 
+              <InputRoot>
+                <InputControl defaultValue="Charleaux"/>
+              </InputRoot>
+            </div>
+            <div />
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="email" className="text-sm font-medium text-zinc-700">Email Address</label>
+            <div className="grid gap-6 grid-cols-1">
+              <InputRoot>
+              <InputPrefix>
+                <Mail className="h-5 w-5 text-zinc-500"/>
+              </InputPrefix>
+                <InputControl id="email" defaultValue="vinicius.charleaux@gmail.com"/>
+              </InputRoot>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="email" className="text-sm font-medium text-zinc-700">
+              Your photo
+              <span className="block text-sm font-normal text-zinc-500 mt-0.5">This will be displayed on your profile.</span>
+            </label>
+            <div className="grid gap-6 grid-cols-1">
+              Your photo
+            </div>
+            <div />
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="role" className="text-sm font-medium text-zinc-700">Role</label>
+            <div className="grid gap-6 grid-cols-1">
+              <InputRoot>
+                <InputControl id="role" defaultValue="CTO"/>
+              </InputRoot>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="country" className="text-sm font-medium text-zinc-700">Country</label>
+            <div >
+            </div>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="timezone" className="text-sm font-medium text-zinc-700">Timezone</label>
+            <div >
+            </div>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="bio" className="text-sm font-medium text-zinc-700">
+              Bio
+              <span className="block text-sm font-normal text-zinc-500 mt-0.5">Write a short introduction</span>
+            </label>
+            <div></div>
+            <div />
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="projects" className="text-sm font-medium text-zinc-700">
+              Portfolio projects
+              <span className="block text-sm font-normal text-zinc-500 mt-0.5">Share a few snippets of your work.</span>
+            </label>
+            <div>
+            </div>
+            <div />
+          </div>
+
+          <div className="flex items-center justify-end pt-5 gap-2">
+            <button type="button" className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50">Cancel</button>
+            <button type="submit" className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700" >Save</button>
+          </div>
         </form>
-
       </div>
       
     </>
