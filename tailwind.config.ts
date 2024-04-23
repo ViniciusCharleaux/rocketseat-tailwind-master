1f9a860a96fb0109ace954f5619cb25dbca230b2
Vinicius Carneiro Charleaux <vinicius.charleaux@gmail.com>
2024-04-22 23:56:23 -0300

    config fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,26 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/**/*.tsx",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
-        profile: 'max-content 1fr max-content',
-        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)'
+        form: 'minmax(7.5rem, 17.5rem), minmax(25rem, 1fr), minmax(0, 15rem)',
       },
+
       borderWidth: {
-        6: '6px'
+        6: '6px',
       },
+
       colors: {
         violet: {
-          25: '#fcfaff'
+          25: '#FCFAFF',
         },
+
         error: {
           25: '#FFFBFA',
           50: '#FEF3F2',
@@ -30,7 +33,7 @@ const config: Config = {
           700: '#B42318',
           800: '#912018',
           900: '#7A271A',
-        }
+        },
       },
 
       keyframes: {
@@ -38,17 +41,18 @@ const config: Config = {
           from: { opacity: 0, transform: 'translateY(-2px)' },
           to: { opacity: 1, transform: 'translateY(0)' },
         },
-        // slideUpAndFade: {
-        //   from: { opacity: 1 },
-        //   to: { opacity: 0 },
-        // },
+        slideUpAndFade: {
+          from: { opacity: 0, transform: 'translateY(2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
       },
+
       animation: {
-        slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-      }
-      
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
